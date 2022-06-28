c61501d06390984b9b0b6ec7fdf2a7b1467f3271
Kisalay <kisalaykisu@gmail.com>
2022-06-28 17:04:12 +0530

    UC1 has been added

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -1,7 +1,27 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class Generics {
-    public static void main(String[] args){
-        System.out.println("Welcome to the Generics program");
+    public static void main(String[] args){ //Entry Point of code
+        Scanner scanner = new Scanner(System.in); //Make scanner obj
+        System.out.println("Enter three numbers: ");
+        Integer x = scanner.nextInt();  //INPUT Int
+        Integer y = scanner.nextInt();
+        Integer z = scanner.nextInt();
+        Integer max = -2147483648;   // ( or int myMaxIntValue= Integer.MAX_VALUE)
+        int result = x.compareTo(max); // method compares the given string with the current string
+        if (result == 1) {
+            max = x;
+        }
+        result = y.compareTo(max);
+        if (result == 1) {
+            max = y;
+        }
+        result = z.compareTo(max);
+        if (result == 1) {
+            max = z;
+        }
+        System.out.println("The max number is : " + max);
     }
 }
