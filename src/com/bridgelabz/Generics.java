26ba943a2d9c8e06f9f50eaf20a33840d6c66497
Kisalay <kisalaykisu@gmail.com>
2022-06-28 17:09:07 +0530

    UC 3 has been added

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 public class Generics {
     public static void main(String[] args){ //Entry Point of code
-        Scanner scanner = new Scanner(System.in); //Make scanner obj
-        System.out.println("Enter three numbers: ");
-        Integer x = scanner.nextInt();  //INPUT Int
-        Integer y = scanner.nextInt();
-        Integer z = scanner.nextInt();
-        Integer max = -2147483648;   // ( or int myMaxIntValue= Integer.MAX_VALUE)
+        Scanner scanner = new Scanner(System.in); // make scanner obj
+        System.out.println("Enter three strings: ");
+        String x = scanner.next();  // Input string
+        String y = scanner.next();
+        String z = scanner.next();
+        String max = "";
         int result = x.compareTo(max); // method compares the given string with the current string
-        if (result == 1) {
+        if (result > 1) {
             max = x;
         }
         result = y.compareTo(max);
-        if (result == 1) {
+        if (result > 1) {
             max = y;
         }
         result = z.compareTo(max);
-        if (result == 1) {
+        if (result > 1) {
             max = z;
         }
-        System.out.println("The max number is : " + max);
+        System.out.println("The max String is : " + max);
     }
 }
