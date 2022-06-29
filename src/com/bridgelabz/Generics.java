8765daf2dbf1b958662e0fbf8da47ab9e3c195ed
Kisalay <kisalaykisu@gmail.com>
2022-06-29 18:17:59 +0530

    UC5 has been added

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -1,26 +1,44 @@
 package com.bridgelabz;
+import java.util.Arrays;
 
-public class Generics {
-    /*
-      E This represents generic types . this declaration has to do nothing with return type
-     */
-    public static  <E extends Comparable> E getMaximum(E a, E b, E c){
-        E max=a;
-        if(b.compareTo(a)>0 && b.compareTo(c)>0){  // compares the given string with the current
-            max=b;
-        }else if(c.compareTo(max)>0){
-            max=c;
+public class Generics <T extends Comparable<T>> { //sorting using comparable interface in collection generic class
+        private T[] inputArray;
+
+        Generics(T[] inputArray) { //generics method to accept Type from generics class array
+            this.inputArray = inputArray;//converting into this method object
         }
-        return max;
-    }
-    public static void main(String[] args){ //Entry Point of code
-        System.out.println("Welcome to Test Maximum Program");
-        int num1=196,num2=156,num3=659; // taking 3 value for int
-        System.out.println(getMaximum(num1,num2,num3));
-        float a=76.7f,b=26.6f,c=98.5f; // taking 3 value for float
-        System.out.println(getMaximum(a,b,c));
-        String s1="Kisalay",s2="santosh",s3="bridgelabz";  // taking 3 value for string
-        System.out.println(getMaximum(s1,s2,s3));
 
-    }
+        public void maxElement() {//accept more than 3 no using array
+            for (int i = 0; i < inputArray.length - 1; i++) { //index value of array
+                if (inputArray[i].compareTo(inputArray[i + 1]) > 0) {
+                    inputArray[i + 1] = inputArray[i];
+                }
+            }
+            System.out.println(inputArray[inputArray.length - 1]);
+        }
+        public void display(){
+            for(int i=0;i<inputArray.length;i++){
+                System.out.println(inputArray[i]);
+            }
+            Arrays.sort(inputArray);
+            System.out.println("After sorting");
+            for(int i=0; i<inputArray.length;i++){
+                System.out.println(inputArray[i]);
+            }
+
+        }
+
+        public static void main(String[] args) {
+            // TODO Auto-generated method stub
+            Integer[] i = {56, 30, 70, 123, 43, 56};
+            String[] s = {"Peach", "Banana", "Apple", "Orange"};
+            Float[] f = {0.56f, 000000.30f, 7.340f, 0000.01f};
+            new Generics<>(i).maxElement();
+            new Generics(s).maxElement();
+            new Generics(f).maxElement();
+            new Generics(i).display();
+            new Generics(s).display();
+            new Generics(f).display();
+
+        }
 }
